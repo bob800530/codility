ff7a104e6d8db8f99ef0cb3bac7cba60bf02402f
bob800530 <bob800530@gmail.com>
2018-10-09 19:26:55 +0800

    Add Ch1 solution

---
+++ Ch1-Iterations/BinaryGap.cpp
@@ -0,0 +1,29 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(int N) {
+    // write your code in C++14 (g++ 6.2.0)
+    int count = 0;
+    int max = 0;
+    bool findOne = false;
+    while(N>0)
+    {
+        if((N&0x1)==1)
+        {
+            findOne = true;
+            count = 0;
+        }
+        else
+        {
+            if(findOne)
+                count++;
+        }
+        if(count>max)
+           max = count; 
+        N = N>>1;
+    }
+    return max;
+}
